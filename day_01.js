9f851b3d9d46557eecadb3ccc0a7b5f9f93362e1
Dipanshu Zalke <dipanshuzalke@gmail.com>
2024-07-15 00:12:02 +0530

    first commmit

---
+++ day_01.js
@@ -0,0 +1,47 @@
+//Activity 1 : Variable Declaration
+//Task 1
+var a = 10;
+console.log(a);
+
+//Task 2
+let str = "abc";
+console.log(str);
+
+//Activity 3 : Constant declaration
+//Task 3
+const isTrue = true;
+console.log(isTrue);
+
+//Activity 4 : Data Types
+//Task 4
+let num = 10;  //number
+console.log(typeof num);
+
+let string = "abc";  //string
+console.log(typeof string);
+
+let isFalse = false;  //boolean
+console.log(typeof isFalse);
+
+let student = {  //object
+    name : "dipanshu",
+    age : 19,
+    marks : 95
+};
+console.log(typeof student);
+
+let arr = ["abc", "pyq", "xyz"];  //array
+console.log(typeof arr);
+
+//Activity 4 : Reassigning values
+//Task 5
+let value = 7;
+console.log(`value org : ${value}`);
+value = 4;
+console.log(`value new : ${value}`);
+
+//Activity 5 : Understanding const
+//Task 6
+const age = 19;
+age = 20;
+console.log(age);
